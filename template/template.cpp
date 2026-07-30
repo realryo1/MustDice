f6a368a4ab92102537251b9a09e8a48f904a946e
realryo1 <realryo1@gmail.com>
2026-07-31 07:13:14 +0900

    template更新

--- template/template.cpp
+++ template/template.cpp
@@ -1,14 +1,16 @@
-﻿#include "template.h"
+﻿#include "Template.h"
 #include "define.h"
 #include "font.h"
+#include "clickfont.h"
 #include "input_manager.h"
 #include "fade.h"
-#include "scene.h"
 
 using namespace DirectX;
 
+static Sprite2D* g_pNaiyo = nullptr;
 static DrawFont* g_pTemplateText = nullptr;
 static DrawFont* g_pHintText = nullptr;
+static ClickFont* g_pDebugButton = nullptr;
 
 void Template_Initialize(void)
 {
@@ -17,7 +19,7 @@ void Template_Initialize(void)
 		48.0f,
 		0.0f,
 		{ 1.0f, 1.0f, 1.0f, 1.0f },
-		"TEMPLATE"
+		"Template"
 	);
 
 	g_pHintText = new DrawFont(
@@ -27,25 +29,62 @@ void Template_Initialize(void)
 		{ 0.8f, 0.8f, 0.8f, 1.0f },
 		"Press Decide"
 	);
+
+	g_pNaiyo = new Sprite2D(
+		{ 140.0f, 140.0f },
+		{ 200.0f, 200.0f },
+		0.0f,
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		BLENDSTATE_NONE,
+		L"asset\\texture\\notfound_thumbnail.png"
+	);
+
+	g_pDebugButton = new ClickFont(
+		{ SCREEN_WIDTH - 100.0f , 50.0f },
+		20.0f,
+		0.0f,
+		{ 0.8f, 0.8f, 0.8f, 1.0f },
+		{ 0.8f, 0.8f, 0.8f, 0.5f },
+		"[debugscene]"
+	);
 }
 
 void Template_Update(void)
 {
+	// ===================== 入力操作について =====================
+	//
+	// コントローラー、キーボード双方の入力の実装をやりやすくするため、原則keyboard.hを直接読み取ることはしない
+	// input_manager.hのInput_IsActionDown,Triggerを使用する。
+	// document\input.mdを参照のこと。
+	//
+	// ==========================================================
+
 	if (Input_IsActionTrigger(INPUT_ACTION_DECIDE))
 	{
-		// 遷移先シーンは用途に合わせて変更する
-		SetSceneFade(SCENE_TITLE);
+		SetSceneFade(SCENE_Template);
 	}
+
+	//適当にぐるぐる
+	g_pTemplateText->AddRot(360.0f * (1.0f / FPS / 4));
+	g_pNaiyo->AddRot(-360.0f * (1.0f / FPS / 4));
+
+	//clickfontはupdateとisclick両方必要
+	g_pDebugButton->Update();
+	if (g_pDebugButton->IsClick())SetSceneFade(SCENE_DEBUG);
 }
 
 void Template_Draw(void)
 {
 	if (g_pTemplateText) g_pTemplateText->Draw();
 	if (g_pHintText) g_pHintText->Draw();
+	if (g_pNaiyo) g_pNaiyo->Draw();
+	if (g_pDebugButton) g_pDebugButton->Draw();
 }
 
 void Template_Finalize(void)
 {
 	SAFE_DELETE(g_pTemplateText);
 	SAFE_DELETE(g_pHintText);
+	SAFE_DELETE(g_pNaiyo);
+	SAFE_DELETE(g_pDebugButton);
 }
